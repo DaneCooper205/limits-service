65a230776f8b8ff0c0171b4072bdd2512a66ebc1
Dane Cooper <dane.cooper@use.startmail.com>
2024-02-23 20:48:52 -0600

    Intial Setup

---
+++ limits-service/src/test/java/com/in28minuntes/rest/webservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minuntes.rest.webservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
